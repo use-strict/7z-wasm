afb0a52c15b2bbea6de3a26f049c775bf212bf0a
Alexandru Ciuca <gmail.rulz@yahoo.com>
2021-09-04 15:00:46 +0300

    Fix read from stdin
    Fixes #2

--- cli.js
+++ cli.js
@@ -1,8 +1,28 @@
 var path = require("path");
 var SevenZip = require("./7zz.umd");
+var readlineSync = require("readline-sync");
+
+var buf;
+var i = 0;
 
 SevenZip({
-    quit: (code) => {
+    stdin: () => {
+        if (!buf) {
+            buf = readlineSync.question() + "\n";
+        }
+        if (i < buf.length) {
+            return buf.charCodeAt(i++);
+        }
+        buf = void 0;
+        i = 0;
+        return null;
+    },
+    stdout: (charCode) => {
+        if (charCode !== null) {
+            process.stdout.write(String.fromCharCode(charCode));
+        }
+    },
+	quit: (code) => {
         if (code) {
             process.exit(code);
         }
@@ -12,7 +32,7 @@ SevenZip({
     var hostRoot = path.parse(cwd).root;
     var hostDir = path.relative(hostRoot, cwd).split(path.sep).join("/");
     var mountRoot = "/nodefs";
-    sevenZip.FS.mkdir(mountRoot);
+	sevenZip.FS.mkdir(mountRoot);
     sevenZip.FS.mount(sevenZip.NODEFS, { root: hostRoot }, mountRoot);
     sevenZip.FS.chdir(mountRoot + "/" + hostDir);
 
