db881f8741a5d5743c8731efbd21a97a9a162b10
Alexandru Ciuca <gmail.rulz@yahoo.com>
2022-08-30 13:32:56 +0300

    Update 7-Zip to 22.01

--- build.cmd
+++ build.cmd
@@ -1,5 +1,7 @@
 docker build --force-rm -t 7z-build . &&^
 docker run --rm -it -v "%CD%":/app -w /app --env-file build-umd.env 7z-build bash -e build_in_docker.sh &&^
+del 7zz.umd.js &&^
 rename 7zz.js 7zz.umd.js &&^
 docker run --rm -it -v "%CD%":/app -w /app --env-file build-es6.env 7z-build bash -e build_in_docker.sh &&^
+del 7zz.es6.js &&^
 rename 7zz.js 7zz.es6.js
